02b7eae104ccdbdb98865ee55fc01f7c740a785b
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-09-21 16:29:10 -0700

    single page, removed routes to blog

--- routes.js
+++ routes.js
@@ -1,6 +1,4 @@
 const nextRoutes = require("next-routes");
 const routes = (module.exports = nextRoutes());
 
-routes.add("blog", "/:language/blog");
-routes.add("blog/detail", "/:language/blog/:slug");
 routes.add("index", "/");
